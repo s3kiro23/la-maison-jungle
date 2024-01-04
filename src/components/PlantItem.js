ac70fe0390ccfe537eea80f4ded714ed3bc88700
Jean-Marie GERONIMI <geronimi@goodbarber.com>
2024-01-04 15:29:59 +0100

    add styled components

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -1,9 +1,74 @@
 import CareScale from "./CareScale";
 import "../styles/PlantItem.css";
 import Swal from "sweetalert2";
+import styled, { css } from "styled-components";
+import { useState } from "react";
+
+const Button = styled.button`
+	background: transparent;
+	border-radius: 15px;
+	border: 2px solid #31b472;
+	color: #31b472;
+	font-weight: bold;
+	padding: 0.75em 1em;
+	cursor: pointer;
+	width: 100%;
+
+	&:hover {
+		background: #31b472;
+		color: white;
+		transition: 0.3s;
+		box-shadow: 0 0 10px #31b472;
+		transform: scale(1.05);
+		transition: 0.3s;
+	}
+
+	&:active {
+		transform: scale(0.95);
+		transition: 0.3s;
+		box-shadow: none;
+	}
+`;
+
+const Img = styled.img`
+	height: 250px;
+	width: 250px;
+	border-radius: 20px;
+	object-fit: cover;
+	cursor: pointer;
+
+	&:hover {
+		transform: scale(1.05);
+		transition: 0.3s;
+		box-shadow: 0 0 10px #31b472;
+	}
+`;
+
+const Soldes = styled.div`
+	${({ isImgHovered }) =>
+		isImgHovered &&
+		css`
+			top: -6px;
+			left: -5px;
+			transform: scale(1.05);
+			transition: 0.3s;
+		`}
+`;
+
+const Price = styled.div`
+	${({ isImgHovered }) =>
+		isImgHovered &&
+		css`
+			bottom: 32.8%;
+			right: -5px;
+			transform: scale(1.05);
+			transition: 0.3s;
+		`}
+`;
 
 function PlantItem(props) {
 	const plant = props.plant;
+	const [isImgHovered, setIsImgHovered] = useState(false);
 
 	function handleCareType(careType, scaleValue) {
 		const careTypeFinal = careType === "light" ? "de lumière" : "d'arrosage";
@@ -40,6 +105,7 @@ function PlantItem(props) {
 			showCloseButton: true,
 			showCancelButton: true,
 			focusConfirm: false,
+			confirmButtonColor: "#31b472",
 			confirmButtonText: `
 			  <i class="fa fa-thumbs-up"></i> Great!
 			`,
@@ -53,15 +119,26 @@ function PlantItem(props) {
 
 	return (
 		<li className="lmj-plant-item">
-			<img src={plant.cover} alt={`${plant.name} cover`} className="lmj-plant-item-cover" onClick={() => modalSwal(plant)} />
-			<div className="lmj-plant-item-price">{plant.price}€</div>
+			<Img
+				src={plant.cover}
+				alt={`${plant.name} cover`}
+				className="lmj-plant-item-cover"
+				onClick={() => modalSwal(plant)}
+				onMouseEnter={() => setIsImgHovered(true)}
+				onMouseLeave={() => setIsImgHovered(false)}
+			/>
+			<Price className="lmj-plant-item-price" isImgHovered={isImgHovered}>{plant.price}€</Price>
 			{plant.name}
-			{plant.isSpecialOffer && <div className="lmj-sales">Soldes</div>}
+			{plant.isSpecialOffer && (
+				<Soldes className="lmj-sales" isImgHovered={isImgHovered}>
+					Soldes
+				</Soldes>
+			)}
 			<div className="lmj-carescale">
 				<CareScale careType="water" scaleValue={plant.water} />
 				<CareScale careType="light" scaleValue={plant.light} />
 			</div>
-			<button onClick={() => props.addToCart(plant.name, plant.price)}>Ajouter</button>
+			<Button onClick={() => props.addToCart(plant.name, plant.price)}>Ajouter</Button>
 		</li>
 	);
 }
